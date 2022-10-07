fca9f03007647737fba362198a97d1b6d02e1c62
Ayanara <aya.nathane@gmail.com>
2022-10-07 17:25:01 -0300

    requisito 1

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  name: '', // nome-da-pessoa
+  assertions: '', // número-de-acertos
+  score: '', // pontuação
+  gravatarEmail: '', // email-da-pessoa
+};
+
+function user(state = INITIAL_STATE) {
+  return state;
+}
+
+export default user;
