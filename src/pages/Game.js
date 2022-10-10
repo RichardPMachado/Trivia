e00d3bf401d18f76d05c5620022e822ea8cc870f
RichardPMachado <richardmachado2211@gmail.com>
2022-10-10 16:56:40 -0300

    refatoração

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,141 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { fetchGame } from '../redux/actions';
+import Loading from '../components/Loading';
+import './Game.css';
 
 class Game extends React.Component {
+  state = {
+    questions: [],
+    correctAnswer: '',
+    answers: [],
+    // isLoading: true,
+    isRedirect: false,
+    isDisabledColorButton: true,
+    // isDisabled: true,
+    borderColorButton: [],
+  };
+
+  componentDidMount() {
+    this.fetchGameQuest();
+  }
+
+  fetchGameQuest = async () => {
+    const { fetchGameDispatch } = this.props;
+    await fetchGameDispatch();
+    const { questionsProps } = this.props;
+    const {
+      results,
+      response_code: responseCode,
+    } = questionsProps;
+    if (responseCode === 0) {
+      const numberAux = 0.5;
+      const toSorted = [results[0].correct_answer, ...results[0].incorrect_answers];
+      const randomSequence = toSorted.sort(() => Math.random() - numberAux);
+      // console.log(randomSequence);
+      this.setState({
+        questions: questionsProps,
+        correctAnswer: results[0].correct_answer,
+        answers: randomSequence,
+      });
+    } else {
+      localStorage.clear();
+      this.setState({ isRedirect: true });
+      throw new Error('Token Invalid');
+    }
+  };
+
+  // move = () => {
+  //   const id = setInterval(frame, 10);
+  //   function frame() {
+  //     if (width == 100) {
+  //       clearInterval(id);
+  //     } else {
+  //       width++;
+  //       element.style.width = width + '%';
+  //     }
+  //   }
+  // }
+
+  // handleClick = (correctAnswer, answers) => {
+  //   let aux = [];
+  //   answers.forEach((element) => {
+  //     if (correctAnswer === element) {
+  //       aux = [...aux, 'correct-answer'];
+  //     } else {
+  //       aux = [...aux, 'incorrect-answer'];
+  //     }
+  //   });
+  //   this.setState({ borderColorButton: [...aux], isDisabledColorButton: false });
+  // };
+
   render() {
+    const { correctAnswer, answers, isRedirect, questions,
+      isDisabledColorButton, borderColorButton } = this.state;
+    console.log(questions);
+
+    const { isLoadingProps } = this.props;
     return (
       <div>
         <Header />
+        {isLoadingProps ? <Loading />
+          : (
+            <section>
+              <p data-testid="question-category">{questions[0].category}</p>
+              <p data-testid="question-text">{ questions[0].question}</p>
+              <span data-testid="answer-options">
+                {
+                  answers.map((answer, index) => (
+                    <button
+                      type="button"
+                      key={ index }
+                      className={ isDisabledColorButton
+                        ? 'button' : borderColorButton[index] }
+                      onClick={ () => this.handleClick(correctAnswer, answers) }
+                      data-testid={
+                        answer === correctAnswer
+                          ? 'correct-answer' : `wrong-answer-${index}`
+                      }
+                    >
+                      { answer }
+                    </button>
+                  ))
+                }
+              </span>
+            </section>
+          )}
+        {isRedirect && <Redirect to="/" />}
       </div>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+//   emailProps: state.playerReducer.gravatarEmail,
+//   nameProps: state.playerReducer.name,
+  questionsProps: state.gameReducer.questions,
+  isLoadingProps: state.playerReducer.isLoading,
+  // isRedirectProps: state.playerReducer.isRedirect,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  // dispatchError: () => dispatch(actLogout()),
+  fetchGameDispatch: () => dispatch(fetchGame()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
+
+Game.propTypes = {
+  resultsProps: PropTypes.shape({
+    responseCode: PropTypes.number,
+    questionsProps: PropTypes.shape({
+      results: PropTypes.shape(PropTypes.objectOf),
+      responseCode: PropTypes.number,
+    }),
+  }).isRequired,
+  isLoading: PropTypes.bool,
+  isRedirect: PropTypes.bool,
+}.isRequire;
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,20 +4,27 @@ export const INITIAL_REQ = 'INITIAL_REQ';
 export const FINAL_REQ = 'FINAL_REQ';
 export const TOKEN_REQUEST = 'TOKEN_REQUEST';
 export const GET_USER = 'GET_USER';
+export const ACTION_LOGOUT = 'ACTION_LOGOUT';
+export const FIVE_NEW_QUESTIONS = 'FIVE_NEW_QUESTIONS';
+
 const endPointToken = 'https://opentdb.com/api_token.php?command=request';
 
 const initialRequest = () => ({ type: INITIAL_REQ });
 const tokenRequest = (payload) => ({ type: TOKEN_REQUEST, payload });
-const finalRequest = () => ({ type: FINAL_REQ });
+
 const getUser = (payload) => ({ type: GET_USER, payload });
 
+const actLogout = () => ({ type: ACTION_LOGOUT });
+const questionsLoaded = (payload) => ({ type: FIVE_NEW_QUESTIONS, payload });
+const finalRequest = () => ({ type: FINAL_REQ });
+
 const getApiToken = () => async (dispatch) => {
-  // dispatch(initialRequest());
+  dispatch(initialRequest());
   try {
     const response = await fetch(endPointToken);
     const token = await response.json();
     dispatch(tokenRequest(token));
-    // dispatch(finalRequest());
+    dispatch(finalRequest());
   } catch (e) {
     throw new Error(e);
   }
@@ -28,6 +35,30 @@ const criarImg = (email) => {
   return `https://www.gravatar.com/avatar/${hash}`;
 };
 
+const fetchGame = () => async (dispatch) => {
+  dispatch(initialRequest());
+  try {
+    const token = localStorage.getItem('token');
+    const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const results = await response.json();
+    // const {
+    //   results,
+    //   response_code: responseCode,
+    // } = await response.json();
+    // if (responseCode === 0) {
+    // console.log(results);
+    // return { results, responseCode };
+    dispatch(questionsLoaded(results));
+    dispatch(finalRequest());
+  } catch (e) {
+    localStorage.clear();
+    dispatch(actLogout());
+    throw new Error(e);
+  }
+  // return { results, responseCode };
+  // throw new Error('Token Invalid');
+};
+
 export {
   criarImg,
   getApiToken,
@@ -35,4 +66,7 @@ export {
   finalRequest,
   tokenRequest,
   getUser,
+  questionsLoaded,
+  fetchGame,
+  actLogout,
 };
