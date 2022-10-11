afb8d86a0c37e5338b707a96902ad5f003a76059
JohnnyNWT <jooaaooskt@gmail.com>
2022-10-11 16:51:46 -0300

    Requisito 19 - (Ainda falta implementar o local Storage)

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { criarImg } from '../redux/actions/index';
 import Header from '../components/Header';
 
 class Ranking extends React.Component {
@@ -9,11 +11,20 @@ class Ranking extends React.Component {
   };
 
   render() {
+    const { playerName, gravatarEmail, score } = this.props;
+    const profile = criarImg(gravatarEmail);
     return (
       <div>
         <Header />
         <div>
           <h1 data-testid="ranking-title">Ranking</h1>
+          <img
+            src={ profile }
+            alt={ `Imagem do jogador ${playerName}` }
+          />
+          <h3>{ playerName }</h3>
+          <h3>{ score }</h3>
+
           <button
             type="button"
             data-testid="btn-go-home"
@@ -27,6 +38,13 @@ class Ranking extends React.Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  playerName: state.player.name,
+  gravatarEmail: state.player.gravatarEmail,
+  score: state.player.score,
+});
+
 Ranking.propTypes = {
   history: PropTypes.array,
 }.isRequired;
@@ -37,4 +55,4 @@ Ranking.propTypes = {
 //   }).isRequired,
 // });
 
-export default Ranking;
+export default connect(mapStateToProps)(Ranking);
